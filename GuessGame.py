767995f1efaa601331090c463f9b76dd885695b7
david <43478590+danielnotexist@users.noreply.github.com>
2022-07-11 16:35:34 +0300

    Guess game is up! Version - 1.0.3

--- GuessGame.py
+++ GuessGame.py
@@ -1,15 +1,20 @@
-
 import random as rd
 
 # Set variables
-counter = 0
+counter = 1
 tries = 5
 print("******* Welcome to GuessGame! *******")
+
+# The user selects a start number and an end number to select the range of the target number
 start_target = int(input("Please enter the start target number of range:- "))
-last_target = int(input("Please enter the last target number of range:- "))
-target = rd.randint(start_target, last_target)
-print(f"You have only {tries} chance to guess the number between {start_target} to {last_target}!")
+end_target = int(input("Please enter the last target number of range:- "))
+target = rd.randint(start_target, end_target)
+print(f"You have only {tries} chance to guess the number between {start_target} to {end_target}!")
+
+# The user has to guess the number
 user_choose = int(input("please enter your guess:- "))
+
+# While loop to test the user input
 while user_choose != target:
     if user_choose < target:
         print("Too Low!")
@@ -19,6 +24,8 @@ while user_choose != target:
     user_choose = int(input("please enter your guess:- "))
     if counter == tries:
         break
+# End of the whille loop
+
 if counter <= tries:
     print(f"You Did it! in {counter} Times!")
 else:
